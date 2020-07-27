58722277a9b3b530dea453d085021ea74dd392c6
Barry Keegan <barry.keegan@gmail.com>
2020-07-27 10:57:42 +0100

    added tests for gaussian from course

--- test_distributions.py
+++ test_distributions.py
@@ -1,5 +1,28 @@
+import unittest
 from Gaussian import Gaussian
 
+class TestGaussianClass(unittest.TestCase):
+    def setUp(self):
+        self.gaussian = Gaussian(25, 2)
+
+    def test_initialization(self):
+        self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
+        self.assertEqual(self.gaussian.stdev, 2, 'incorrect standard deviation')
+
+    def test_pdf(self):
+        self.assertEqual(round(self.gaussian.pdf(25), 5), 0.19947,\
+         'pdf function does not give expected result')
+
+    def test_meancalculation(self):
+        self.gaussian.read_data_file('numbers.txt', True)
+        self.assertEqual(self.gaussian.calculate_mean(),\
+         sum(self.gaussian.data) / float(len(self.gaussian.data)), 'calculated mean not as expected')
+
+    def test_stdevcalculation(self):
+        self.gaussian.read_data_file('numbers.txt', True)
+        self.assertEqual(round(self.gaussian.stdev, 2), 92.87, 'sample standard deviation incorrect')
+        self.gaussian.read_data_file('numbers.txt', False)
+        self.assertEqual(round(self.gaussian.stdev, 2), 88.55, 'population standard deviation incorrect')
 
 def test_pytest():
     assert True
@@ -16,3 +39,8 @@ def test_Gaussian_specified_values():
     assert myGauss.stdev == 3
     assert len(myGauss.data) == 0
 
+tests = TestGaussianClass()
+
+tests_loaded = unittest.TestLoader().loadTestsFromModule(tests)
+
+unittest.TextTestRunner().run(tests_loaded)
